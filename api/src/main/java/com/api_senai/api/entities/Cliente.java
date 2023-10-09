eedc8608c43346304ecdb234bfd4a713338d0c17
Arthur Giangiarulo <111184548+arthurGiangiarulo@users.noreply.github.com>
2023-10-08 23:10:28 -0300

    Anotacoes

--- api/src/main/java/com/api_senai/api/entities/Cliente.java
+++ api/src/main/java/com/api_senai/api/entities/Cliente.java
@@ -5,12 +5,14 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 @Data
 @Entity
-@EqualsAndHashCode(callSuper=false)
+@EqualsAndHashCode(callSuper = false)
+@Table(name = "clientes")
 public class Cliente extends Pessoa{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
